9e96f626828275e5b2f4ade05aab674854698c36
nmduy <minhduy260694@gmail.com>
2023-01-12 14:27:03 +0700

    Internationalization

--- src/main/java/com/nmd/example/web/config/WebConfig.java
+++ src/main/java/com/nmd/example/web/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.nmd.example.web.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 // De nhan css or config khac thi can khai bao 1 class Config vs annotation la @Configuration de dang ki css
 @Configuration
@@ -16,5 +25,41 @@ public class WebConfig implements WebMvcConfigurer {
 		// .addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.0.0/");
 		registry.addResourceHandler("/resources/css/**").addResourceLocations("classpath:/static/css/");
 	}
+	
+	
+	//Bean messageSource sẽ thực hiện khai báo các file properties
+	//trong ví dụ này mình khai báo các file properties nằm ở folder i18n, có tên bắt đầu bằng messages
+	//Bean localeResolver sẽ xác định ngôn ngữ hiển thị trên view bằng cách nào, ví dụ thông qua session, qua cookies
+	//Trong ví dụ này, mình sẽ xác định ngôn ngữ hiển thị qua cookies với biến myAppLocaleCookies lưu trên trình duyệt
+	//Method addInterceptors sẽ xác định thay đổi ngôn ngữ hiển thị thông qua param name nào trên trình duyệt. Ví dụ trên URL có param: ?language=en thì tiếng anh sẽ được hiển thị
+	
+	@Bean(name = "localeResolver")
+	public LocaleResolver getLocaleResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setCookieDomain("myAppLocaleCookie");
+		resolver.setDefaultLocale(Locale.ENGLISH);
+		
+		// 60 MINUTES
+		resolver.setCookieMaxAge(60*60);
+		return resolver;
+	}
+	
+	@Bean(name = "messageSource")
+    public MessageSource getMessageResource()  {
+        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+         
+        // Đọc vào file i18n/messages_xxx.properties
+        // Ví dụ: i18n/messages_en.properties
+        messageResource.setBasename("classpath:i18n/messages");
+        messageResource.setDefaultEncoding("UTF-8");
+        return messageResource;
+    }
+	
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("language");
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+    }
 
 }
--- src/main/java/com/nmd/example/web/controller/WebController.java
+++ src/main/java/com/nmd/example/web/controller/WebController.java
@@ -1,6 +1,11 @@
 package com.nmd.example.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // RestController: la 1 annotation đánh dấu class là controller, trong controller này sẽ thiết kế các api theo kiến trúc REST: PUT, POST, GET, DELETE
@@ -10,9 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
 
 	@RequestMapping(value = "/index")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("/locale")
+	public String locale(Model model, HttpServletRequest request) {
+		String msg = messageSource.getMessage("hello", null, "default message", request.getLocale());
+		model.addAttribute("msg", msg);
+		return "locale";
 	}
 }
